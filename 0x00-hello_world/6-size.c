5f491590ca97c4d71ab407629dad552aae88cb98
Sanieeme <shoneesani@gmail.com>
2023-07-13 15:04:37 -0700

    6 size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  *main- this is the function
- *Return: 0
+ *Return: 0 on success
  */
 
 int main(void)
@@ -11,7 +11,6 @@ int main(void)
 	printf("size of int: %zu bytes\n", sizeof(int));
 	printf("Size of long int: %zu bytes\n", sizeof(long int));
 	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-	printf("Size of float: %zu bytes\n" sizeof(float));
-
+	printf("Size of float: %zu bytes\n", sizeof(float));
 	return (0);
 }
