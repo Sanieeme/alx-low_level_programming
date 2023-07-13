2b591f0fb67ebd096e0c51c1154ccb9bbe394682
Sanieeme <shoneesani@gmail.com>
2023-07-13 11:16:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - this is the main function
- *
- * return: always 0
+ * Return: always 0
  */
 
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	Return(0);
 }
