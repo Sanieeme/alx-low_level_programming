2bc46ec51a700f0b0dca1f67c7f9c89d422b14b7
Sanieeme <shoneesani@gmail.com>
2023-07-14 15:14:36 -0700

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	for (c = 'a'; c < 'g'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+
+}
