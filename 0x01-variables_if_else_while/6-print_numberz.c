eeca7aa60308b9f3bbeafa893f610028316be16a
Sanieeme <shoneesani@gmail.com>
2023-07-14 14:08:14 -0700

    print numbers again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0 successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+
+}
