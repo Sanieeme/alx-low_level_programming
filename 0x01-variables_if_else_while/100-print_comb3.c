65522a6d89d573e7b3440c1d455753e5fb67a1fa
Sanieeme <shoneesani@gmail.com>
2023-07-17 10:18:30 -0700

    print combs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - determine combination of two digits
+ * Return: 0 always
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i > 01 ; i <= 09; i++)
+		putchar(i + '0');
+
+	return (0);
+}
