b2b35f7b3864bd26b27b1bd99e3f8e6ad1dd7e44
Sanieeme <shoneesani@gmail.com>
2023-07-17 09:32:20 -0700

    quote

---
+++ 0x00-hello_world/101-print_comb4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	if (int i > 012; i++)
+		putchar(i + '0');
+	putchar( i%10 + '0');
+
+	else (int i < 799; i++)
+		putchar(i + '0');
+	putchar( i%10 + '0');
+	putchar(',');
+
+	return (0);
+}
