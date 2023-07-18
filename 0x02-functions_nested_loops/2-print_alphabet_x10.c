fcf4ddf4f422c0119464025c3c946feb81668700
Sanieeme <shoneesani@gmail.com>
2023-07-18 09:45:17 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: 0 always
+ */
+
+void print_alphabet_x10(void)
+{
+	char i, c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+
+}
