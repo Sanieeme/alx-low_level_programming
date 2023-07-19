990647f4afc367bbd3dcb4885d4e1e07c0c70ce3
Sanieeme <shoneesani@gmail.com>
2023-07-19 01:56:33 -0700

    time tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,11 +13,11 @@ void times_table(void)
 
 	for (i =  0 ; i < 10 ; i++)
 	{
-		for (j = 0 ; j < 10 ; i++)
+		for (j = 0 ; j < 10 ; j++)
 		{
 			result = i * j;
 			if (j == 0)
-				printf("%d", result);
+				printf("%d ", result);
 			else
 			{
 				printf("%2d", result);
