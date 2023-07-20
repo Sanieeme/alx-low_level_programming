3445a85c106ae34ce30067da7178fca5cf7e6829
Sanieeme <shoneesani@gmail.com>
2023-07-20 05:57:23 -0700

    printing remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,16 +12,20 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	month = 2;
+	day = 29;
+	year = 2000;
+
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-	for (month >= 2 && day <= 60, day++)
+	for (month >= 2 && day >= 60, day++)
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 2 && day == 60 && year == 2000)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
