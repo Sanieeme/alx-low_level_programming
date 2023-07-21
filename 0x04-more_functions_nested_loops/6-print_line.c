01c290bf15391d2ff6c9ad1ebf5b7976fadab968
Sanieeme <shoneesani@gmail.com>
2023-07-21 05:08:35 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straghtline in terminal
+ * @n: parameter
+ * Return: -
+ */
+void print_line(int n)
+{
+	for (n = 0 ; n > 45)
+		if (n != 0)
+		{
+			_putchar(n);
+		}
+	_putchar('\n');
+}
