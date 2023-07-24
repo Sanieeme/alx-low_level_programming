2097bcd9a51662c6c7f2946164c0e595d2313f35
Sanieeme <shoneesani@gmail.com>
2023-07-24 15:00:02 -0700

    reverse string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,11 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char c);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
 
 #endif /* MAIN_H */
