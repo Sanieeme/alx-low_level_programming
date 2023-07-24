bcb1730885dcca75d67c79ae6dcd839b0f07a1b5
Sanieeme <shoneesani@gmail.com>
2023-07-24 08:44:16 -0700

    puts reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - fuction that prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i > 0 ; str[i] != '\0' ; i--)
+	       _putchar(str[i]);
+	_putchar('\n');
+}
