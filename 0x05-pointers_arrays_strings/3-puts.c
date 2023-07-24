becac6fb40076568b5568a2f07f45e0310fad7da
Sanieeme <shoneesani@gmail.com>
2023-07-24 08:19:50 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - fuction that prints a string
+ * @str: string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
