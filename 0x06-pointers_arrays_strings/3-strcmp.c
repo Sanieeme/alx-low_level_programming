8ff1c6a8691156b54ad1b6f4b961bf1ac25a4f5f
Sanieeme <shoneesani@gmail.com>
2023-07-27 07:02:05 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,13 +2,23 @@
 
 /**
  * _strcmp - function that compares strings
- * s1: string
- * s2: string
- * Return: dest
+ * @s1: parameter
+ * @s2: parameter
+ * Return: int
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i] || s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 
 }
 
