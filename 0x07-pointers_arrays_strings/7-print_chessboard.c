1bc3e7d4d7306b7f38867f061e538ebb5780b5e0
Sanieeme <shoneesani@gmail.com>
2023-08-01 01:54:49 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,13 +2,21 @@
 
 /**
  * print_chessboard - function that prints chessboard
- * *a: 
- * Return: 
+ * @a: pointer
+ * Return: void
  */
- 
- void print_chessboard(char (*a)[8])
-{
-	int r, c;
 
-	for (r = 0 ; r 
+void print_chessboard(char (*a)[8])
+{
+	int row = 8, col = 8, i, j;
+
+	for (i = 1 ; i <= row ; i++)
+	{
+		for (j = 1 ; j <= col ; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+
 }
