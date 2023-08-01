4dd30c7a867b60218091ceaa8faff181c0b3a220
Sanieeme <shoneesani@gmail.com>
2023-08-01 01:39:09 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,8 +19,7 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[i])
 		{
 			do {
-				(needle[i + 1] == '\0')
-
+				if (needle[i + 1] == '\0')
 					return (haystack);
 				i++;
 			} while (haystack[i] == needle[i]);
