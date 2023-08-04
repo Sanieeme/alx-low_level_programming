7fbe6c3ac24898b707b44cce9f229c07134a0590
Sanieeme <shoneesani@gmail.com>
2023-08-04 06:06:52 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,11 +5,11 @@
  * @str: string
  * Return: void
  */
-void print_rev(char *s);
+void _puts(char *str)
 {
 	int i;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
-		_putchar(s[i]);
+	for (i = 0 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
