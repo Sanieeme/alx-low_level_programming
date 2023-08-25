b714c937197dc793c25963d0d1b6d26449daf32c
Sanieeme <shaninie@gmail.com>
2023-08-25 13:16:36 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print - function that prints
+ * Return: void
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
