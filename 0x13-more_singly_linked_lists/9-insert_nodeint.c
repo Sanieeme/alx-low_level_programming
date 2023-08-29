c7d0e21371db7a7885498909eb6fc657c23356cf
Sanieeme <shaninie@gmail.com>
2023-08-29 14:16:02 -0700

    inserts nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		n_node->next = *head;
 		*head = n_node;
-		return (NULL);
+		return (n_node);
 	}
 	for (i = 0; temp && i < idx; i++)
 	{
