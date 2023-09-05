041aa998971b41b5157836b76de9daf49b87e1a3
Sanieeme <shaninie@gmail.com>
2023-09-05 02:51:33 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,15 +9,16 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, i, w, size = 0;
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (!file)
+	if (filename == NULL)
 		return (-1);
 
-	if (*text_content)
+	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			size++;
 	}
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	w = write(file, text_content, size);
 
 	if (file == -1)
