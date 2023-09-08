da4164ab5c19c9eeecb948a740f9c625c51f4c9f
Sanieeme <shoneesani@gmail.com>
2023-09-08 03:56:14 -0700

    copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,90 @@
+#include "main.h"
+#define BUF_SIZE 1024
+/**
+ * main - programs that copy content of one file to another
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int f1, f2, r1, r2;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file _to\n");
+		exit(97);
+	}
+	buffer = malloc(sizeof(char) * BUF_SIZE);
+	if (buffer == NULL)
+		return (0);
+
+	f2 = open(argv[1], O_RDONLY);
+	error_98(f2, buffer, argv[1]);
+	f1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	error_98(f1, buffer, argv[2]);
+
+	do
+
+	{
+		r1 = read(f2, buffer, BUF_SIZE);
+		if (r1 == 0)
+			break;
+		error_98(f2, buffer, argv[1]);
+		r2 = read(f1, buffer, BUF_SIZE);
+		if (r2 == 0)
+			break;
+		error_99(f1, buffer, argv[2]);
+	} while (r1 >= BUF_SIZE);
+	r1 = close(f1);
+	error_100(r1, buffer);
+	r2 = close(f2);
+	error_100(r1, buffer);
+	free(buffer);
+	return (0);
+}
+/**
+ * error_98 - function that checks error at 98
+ * @f1: parameter
+ * @buffer: buffer
+ * @argv: argument vector
+ */
+void error_98(int f1, char *buffer, char *argv)
+{
+	if (f1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv);
+		free(buffer);
+		exit(98);
+	}
+}
+/**
+ * error_99 - function that checks error at 99
+ * @f1: parameter
+ * @buffer: buffer
+ * @argv: argument vector
+ */
+void error_99(int f1, char *buffer, char *argv)
+{
+	if (f1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv);
+		free(buffer);
+		exit(99);
+	}
+}
+/**
+ * error_100 - function that checks error at 100
+ * @f1: parameter
+ * @buffer: buffer
+ */
+void error_100(int f1, char *buffer)
+{
+	if (f1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %d\n", f1);
+		free(buffer);
+		exit(100);
+	}
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,5 +8,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void error_98(int f1, char *file, char *argv);
+void error_99(int f1, char *file, char *argv);
+void error_100(int f1, char *file);
 #endif
