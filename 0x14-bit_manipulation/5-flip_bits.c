2b9b53ad462a3c4819d7310db20d9a0887779107
Sanieeme <Shoneesani@gmail.com>
2023-09-29 16:56:52 -0700

    flips

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * flip_bits returns the number of bits to flip
+ * flip_bits - returns the number of bits to flip
  * @n: parameter
- * #m: parameter
+ * @m: parameter
  * Return: number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
