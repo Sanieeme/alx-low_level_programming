2536e2feced6d63fc5d5074fb6a714546ebaa37b
Sanieeme <Shoneesani@gmail.com>
2023-09-29 01:32:18 -0700

    git

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
- /**
-  * print_binary- prints the binary equivalent of *a decimal number
-  * @n: number to print in bínary
-  * Return: 0
-  */
-void print_binary (unsigned long int n)
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in bínary
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
 {
 	int i, size = 0;
 
