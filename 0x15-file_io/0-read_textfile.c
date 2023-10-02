bdf6d7d131886594386b351c0f2ea4ba51443284
Sanieeme <Shoneesani@gmail.com>
2023-10-02 11:48:40 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * read_textfile function that reads a text file and prints
+ * read_textfile - function that reads a text file and prints
  * it to the P0SIX standard output
  * @filename: pointer
  * @letters: number of letters to be printed
  * Return: letters numbers or 0
  */
-ssize_t read_textfile (const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *ch;
 	ssize_t file;
 	ssize_t c;
 	ssize_t i;
-	
+
 	file = open(filename, 0_RDONLY);
 	if (file == -1)
 		return (0);
-	ch = malloc (sizeof (char) * letters);
+	ch = malloc(sizeof(char) * letters);
 	c = read(file, ch, letters);
-	i = write (STDOUT_FILENO, ch, c);
-	free (ch);
+	i = write(STDOUT_FILENO, ch, c);
+	free(ch);
 	close(file);
 	return (i);
 }
