6285943c0a483e40aff09c2ffd29b2e4e26129ab
Sanieeme <Shoneesani@gmail.com>
2023-10-02 16:36:35 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 			size++;
 	}
 
-	file = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(file, text_content, size);
 	if (file == -1)
 		return (-1);
