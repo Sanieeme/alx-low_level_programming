177a8300ccf1430b285f145408a329c1dc9cf499
Sanieeme <Shoneesani@gmail.com>
2023-10-02 11:37:59 -0700

    file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - finction that creates a file
+ * @filename: filename to be created
+ * @text_content: content
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content);
+{
+	int file, j, w, size= 0; 
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (j = 0; text_content[j] != '\0'; j++)
+			size++;
+	}
+
+	file = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	w = write(file, text_content, size);
+	if (file == -1)
+		return (-1);
+	else if (w == -1)
+		return (-1);
+	close(file);
+	return (1);
+}
