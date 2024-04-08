f5da977f6b580e687d2f779c9f65e808e1eb1a6a
Sanieeme <shoneesani@gmail.com>
2024-04-08 04:29:50 -0700

    linear search

---
+++ 0x1E-search_algorithms/1-main.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+/**
+ * binary_search - function that searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ * @array: pointer to the first element of the array to search
+ * @size:  number of elements
+ * @value: value to search
+ * Return: -1  or NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+}
